812cf59f3ac30382af7ca46edd0df67080807958
rainer <rainer.telesko@fhnw.ch>
2020-01-07 09:12:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lotteryexplicitintent/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.lotteryexplicitintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // GUI controls
+    private EditText etNamePlayer;
+    private Spinner spNumberTips;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Get reference to the EditView
+        etNamePlayer = findViewById(R.id.etNamePlayer);
+        // Get reference to the Spinner
+        spNumberTips = findViewById(R.id.spNumberTips);
+    }
+
+    // EditText should be empty when returning from ResultActivity
+    @Override
+    public void onResume() {
+        super.onResume();
+        etNamePlayer.getText().clear();
+    }
+
+    public void sendData(View view) {
+        // Define Intent
+        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+        // Pass Data
+        intent.putExtra("NAME", etNamePlayer.getText().toString());
+        intent.putExtra("NUMBER_TIPS", spNumberTips.getSelectedItem().toString());
+
+        // Check for empty name
+        if (TextUtils.isEmpty(etNamePlayer.getText().toString()))
+            Toast.makeText(this, "Please enter a username", Toast.LENGTH_SHORT).show();
+        else {
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lotteryexplicitintent/ResultActivity.java
@@ -0,0 +1,53 @@
+package com.example.lotteryexplicitintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    // GUI controls
+    private TextView tvNamePlayer;
+    private TextView tvCostsTips;
+    private Button btReturnToMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        // Get reference to the TextViews
+        tvNamePlayer = (TextView) findViewById(R.id.tvNamePlayer);
+        tvCostsTips = (TextView) findViewById(R.id.tvCostsTips);
+
+        Intent intent = getIntent();
+
+        // Get data
+        String namePlayer = intent.getStringExtra("NAME");
+        String numberTips = intent.getStringExtra("NUMBER_TIPS");
+
+        // Calculation of tip costs
+        int tips = Integer.parseInt(numberTips);
+        double costTips = 2.5 * tips;
+
+        // Set Data in TextViews
+        tvNamePlayer.setText("Player name: " + namePlayer);
+        tvCostsTips.setText("Costs in CHF: " + costTips);
+
+        // Button for returning back to Main Activity
+        btReturnToMain = (Button) findViewById(R.id.btReturnToMain);
+        btReturnToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                // Alternative
+                // onBackPressed();
+            }
+        });
+
+    }
+}
